d1b22859de49459fe613fbd38e5b8304e0c51424
ShukranIsaac <imwakabira@ymail.com>
2022-03-02 17:02:56 +0200

    edit: about component

--- components/about.component.js
+++ components/about.component.js
@@ -11,7 +11,7 @@ const About = ({
             <div className="lg:space-x-5 lg:flex lg:flex-row item-center lg:-mx-4 flex flex-col-reverse text-center lg:text-left">
                 <div className="lg:px-4 lg:mt-12 ">
                     <h1 className="text-2xl font-bold text-gray-900 lg:text-5xl dark:text-white">
-                        @mishukran,
+                        <span className="text-6xl font-bold text-blue-600">@mishukran</span>
                     </h1>
                     <div className="mt-6 text-gray-800 dark:text-white">
                         <p className="mb-4">
